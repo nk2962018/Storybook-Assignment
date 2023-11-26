8a00099667907735747071cac2ec7719067f6c53
Nisha <nkolivia1234@gmail.com>
2023-11-26 20:51:26 +0530

    Display Cards on dashboard

---
+++ src/Constants/constants.js
@@ -0,0 +1,72 @@
+export const cardList = [
+    {
+        info: 
+        {
+          id: "1",
+          eyebrowText:'veg',
+          eyebrowTextColor:'grey',
+          headlineText: "Burger King",
+          headlineTextColor :'blue',
+          descriptionText:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled .',
+          descriptionStyle:'',
+          src: "https://www.burgerking.in/static/media/home-bk-wall.96e21fb4.jpg",
+          alt:'Burger King',
+          item:'bk',
+          href :"https://www.burgerking.in/",
+          target:'_blank',
+          linkText:'know More...'        
+        }
+    },
+
+    {
+        info: 
+        {
+          id: "2",
+          eyebrowText:'Non veg',
+          eyebrowTextColor:'lightgrey',
+          headlineText: "Pizza Hut",
+          headlineTextColor :'green',
+          descriptionText:'2 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled .',
+          src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2b4f62d606d1b2bfba9ba9e5386fabb7",
+          alt:'Pizza Hut',
+          href :"https://online.kfc.co.in/",
+          target:'_blank',
+          linkText:'know More...'        
+        }
+    },
+
+    {
+        info: 
+        {
+          id: "3",
+          eyebrowText:'Non veg',
+          eyebrowTextColor:'lightgrey',
+          headlineText: "Subway",
+          headlineTextColor :'green',
+          descriptionText:'2 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled .',
+          src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/1ace5fa65eff3e1223feb696c956b38b",
+          alt:'Subway',
+          href :"https://online.kfc.co.in/",
+          target:'_blank',
+          linkText:'know More...'        
+        }
+    },
+
+    {
+        info: 
+        {
+          id: "4",
+          eyebrowText:'Non veg,veg,South Indian',
+          eyebrowTextColor:'lightgrey',
+          headlineText: "Nandana Palace",
+          headlineTextColor :'green',
+          descriptionText:'2 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled .',
+          src: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/195876a3181ef63f76e45e3a7b49b585",
+          alt:'Nandana Palace',
+          href :"https://online.kfc.co.in/",
+          target:'_blank',
+          linkText:'know More...'        
+        }
+    }
+      
+]
--- src/stories/Dashboard/Dashboard.jsx
+++ src/stories/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
 import './Dashboard.css';
 import Header from '../Header/Header';
+import { cardList } from '../../Constants/constants';
+import Card from '../Card/Card';
 
 const Dashboard = () => {
     return(
         <>           
             <Header headerStyle='food' text='Food App'/>
+            <div className='card-list'>
+                {
+                    cardList.map(card =>(
+                        <Card  {...card.info} key={card.info.id}/>
+                    ))
+                }
+            </div>
         </>
     )
 }
