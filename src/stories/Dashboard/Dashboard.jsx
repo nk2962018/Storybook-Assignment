41f5b5a6df540642ee00c44676c4c26d79c82b56
Nisha <nkolivia1234@gmail.com>
2023-11-26 15:27:06 +0530

    Create Dashboard component

---
+++ src/stories/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import './Dashboard.css';
+
+const Dashboard = () => {
+    return(
+        <h1 className="dashboard">Landing Page</h1>
+    )
+}
+
+export default Dashboard;
---
+++ src/stories/Dashboard/Dashboard.stories.js
@@ -0,0 +1,15 @@
+import Dashboard from "./Dashboard";
+
+export default {
+    title: 'Dashboard - Landing Page',
+    component : Dashboard,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'fullscreen',
+    },
+}
+
+export const LandingPage = {};
+
+
+
